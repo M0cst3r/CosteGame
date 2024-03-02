2cc39e9576318131e30e567c5c2ad5ef34d13072
Matteus Coste <mattcoste03@gmail.com>
2024-03-02 17:04:49 -0500

    file functionality

--- wordle/GameDriver.java
+++ wordle/GameDriver.java
@@ -2,13 +2,17 @@ package wordle;
 
 import wordle.Display;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class GameDriver {
 
-    public static void main(String [] args) {
-        String[] words = {"HELLO", "WORLD", "LEARN", "CRATE"};
+    public static void main(String[] args) {
+        String[] words = readFile("words.txt");
+        assert words != null;
         String answer = words[new Random().nextInt(words.length)].toUpperCase();
 
         WordleBoard board = new WordleBoard(words);
@@ -21,11 +25,28 @@ public class GameDriver {
         }
         display.print();
         if (board.didWin()) {
-            System.out.println("Congratulations, you guessed correctly");
+            System.out.println("Congratulations, you win!");
         } else {
             System.out.println("Sorry, you did not guess correctly");
         }
         System.out.println("The answer was " + board.getWord());
         sc.close();
+    }
+
+    private static String[] readFile(String filename) {
+        ArrayList<String> wordList = new ArrayList<>();
+        File file = new File(filename);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                String word = sc.nextLine().toUpperCase();
+                wordList.add(word);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + filename);
+            return null;
+        }
+        return wordList.toArray(new String[0]);
     }
 }
